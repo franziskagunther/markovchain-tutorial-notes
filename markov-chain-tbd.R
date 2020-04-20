621290a19696b543333494326eebad1d88ff3cb8
Franziska Günther <Franziska@MacBook-Air-5.fritz.box>
2020-04-20 23:13:55 +0200

    Add option to pay when visiting another player's property

--- markov-chain-tbd.R
+++ markov-chain-tbd.R
@@ -35,15 +35,54 @@ print(weather_sequence)
 
 # EX: Monopoly
 
-num_players <- 4
+determine_visit_cost <- function(property, owner=NULL, dice=NULL, prop_index=NULL) {
+  
+  if (property == "electric company or waterworks") {
+    
+    # check if waterworks and/or electric company
+    property_num <- sum(c(13, 29) %in% which(positions_purchased %in% owner))
+    
+    cat("The owner", owner, "possesses", property_num, "of the electric company / waterworks fields\n")
+    
+    if (property_num == 2) {
+      cost <- 10 * dice
+    } else if (property_num == 1) {
+      cost <- 4 * dice
+    }
+    
+  } else if (property == "station") {
+    
+    # check how many stations the owner has
+    property_num <- sum(c(6, 16, 26, 36) %in% which(positions_purchased %in% owner))
+    
+    cat("The owner", owner, "possesses", property_num, "of the station fields\n")
+    if (property_num == 4) {
+      cost <- 200
+    } else if (property_num == 3) {
+      cost <- 100
+    } else if (property_num == 2) {
+      cost <- 50
+    } else if (property_num == 1) {
+      cost <- 25
+    }
+  } else if (property == "norm") {
+    
+    print("A normal street was stepped onto")
+    
+    cost <- properties_that_can_be_bought$cost[prop_index]
+  }
+  
+  return(cost)
+}
 
+num_players <- 4
 num_games <- 1000 # number of games to play
 num_turns <- rep(1:num_players, 1000)
 
-current_board_position <- integer(num_players) # 4 zeros
+current_board_position <- integer(num_players) 
 go_to_jail_position <- 30 # the go to jail space
 jail_position <- 10 # jail space
-properties_that_can_be_bought <- data.frame(pos=c(1, 3, 5, 6, 8, 9, 11, 12, 13, 14, 15, 16, 18, 19, 21, 23, 24, 25, 26, 27, 28, 29, 31, 32, 34, 35, 37, 39), value=c(60, 60, 200, 100, 100, 120, 140, 150, 140, 160, 200, 180, 180, 200, 220, 220, 240, 200, 260, 260, 150, 280, 300, 300, 320, 200, 350, 400), cost=c(60, 60, 200, 100, 100, 120, 140, 150, 140, 160, 200, 180, 180, 200, 220, 220, 240, 200, 260, 260, 150, 280, 300, 300, 320, 200, 350, 400))
+properties_that_can_be_bought <- data.frame(pos=c(1, 3, 5, 6, 8, 9, 11, 12, 13, 14, 15, 16, 18, 19, 21, 23, 24, 25, 26, 27, 28, 29, 31, 32, 34, 35, 37, 39), value=c(60, 60, 200, 100, 100, 120, 140, 150, 140, 160, 200, 180, 180, 200, 220, 220, 240, 200, 260, 260, 150, 280, 300, 300, 320, 200, 350, 400), cost=c(2, 4, NA, 6, 6, 8, 10, NA, 10, 12, NA, 14, 14, 16, 18, 18, 20, NA, 22, 22, NA, 24, 26, 26, 28, NA, 35, 50))
 
 time_to_buy_all_properties <- rep(0, num_games) # vector to store number of turns to buy all properties
 account <- rep(1500, 4)
@@ -52,15 +91,16 @@ account <- rep(1500, 4)
 for ( game in 1:num_games ) {
   
   # move.size <- rep(0, num_turns)
-  positions_visited <- rep(0, length(num_turns)) # 4000 zeros
-  positions_purchased <- rep(0, 40) # repeat zero 40 times
-  properties_bought <- rep(0, length(num_turns)) # 4000 zeros
+  positions_visited <- rep(0, length(num_turns)) 
+  positions_purchased <- rep(0, 40) 
+  properties_bought <- rep(0, length(num_turns)) 
   
   # use a for loop to simulate a number of turns
   for ( turn in 1:length(num_turns) ) {
     
     # determine whose turn it is
     whose_turn <- num_turns[turn]
+    cat("It's player", whose_turn, "'s turn\n")
     
     # roll two dice
     die.values <- sample(c(1:6), 2, replace=TRUE)
@@ -69,38 +109,68 @@ for ( game in 1:num_games ) {
     
     # number of positions to move
     plus.move <- sum(die.values)
+    cat("Rolled a", plus.move, "\n")
     
     # compute new board position
     new_board_position <- current_board_position[whose_turn] + plus.move
     
-    cat(plus.move, "number on dies\n")
-    
     # if Go is passed, increase account by 200
     if ((current_board_position[whose_turn] %% 40 >= 28) && (new_board_position %% 40 <= 11)) {
-      cat(current_board_position[whose_turn], " current position\n")
-      cat(new_board_position %% 40, " position obtained\n")
       account[whose_turn] <- account[whose_turn] + 200
+      cat("Passed GO, ledger is on", account[whose_turn], "\n")
+    }
+    
+    # decrease money on account by 100 for super tax
+    if(new_board_position == 38) {
+      account[whose_turn] <- account[whose_turn] - 100
+      cat("Super Tax, ledger is on", account[whose_turn], "\n")
     }
     
     # if land on GO TO JAIL square, then go backwards to the JAIL square
     if ( new_board_position == go_to_jail_position ) {
       new_board_position = jail_position
+      cat("Player", whose_turn, "went to jail")
     }    
     
     # update board position (this corrects for the fact the board is circular)
     current_board_position[whose_turn] = ( new_board_position %% 40 ) 
+    cat("Moves to board field number", current_board_position[whose_turn], "\n")
     
-    # if we can on a square that can be purchased and which has not been purchased (note R uses 1-indexing for arrays)
+    # if we step on a square that can be purchased and which has not been purchased (note R uses 1-indexing for arrays)
     if ( positions_purchased[current_board_position[whose_turn]+1] == 0 ) {
+      
       if ( current_board_position[whose_turn] %in% properties_that_can_be_bought$pos ) {
+        
         prop_index <- which(properties_that_can_be_bought$pos %in% current_board_position[whose_turn])
-        cat("Balance before: ", account[whose_turn], "\n")
+        
         if(account[whose_turn] >= properties_that_can_be_bought$value[prop_index]) { # buy if we have enough money
+          
           account[whose_turn] <- account[whose_turn] - properties_that_can_be_bought$value[prop_index]
+          
           positions_purchased[current_board_position[whose_turn]+1] <- whose_turn
+          cat("Player", whose_turn, "purchased property with field number", properties_that_can_be_bought$pos[prop_index], "and payed", properties_that_can_be_bought$value[prop_index], "\n")
         } 
-        cat("Balance after: ", account[whose_turn], "\n")
-        cat("Property value: ", properties_that_can_be_bought$value[prop_index], "\n")
+      }
+    } else {
+      whose_property <- positions_purchased[current_board_position[whose_turn]+1]
+      cat("Player", whose_turn, "just stepped onto a property belonging to", whose_property, "\n")
+      
+      if ( current_board_position[whose_turn] %in% properties_that_can_be_bought$pos ) {
+        
+        prop_index <- properties_that_can_be_bought[which(properties_that_can_be_bought$pos %in% current_board_position[whose_turn]), ] 
+        
+        if(prop_index$pos %in% c(12, 28)) {
+          cost <- determine_visit_cost(property="electric company or waterworks", owner=whose_property, dice=plus.move)
+        } else if (prop_index$pos %in% c(5, 15, 25, 35)) {
+          cost <- determine_visit_cost(property="station", owner=whose_property)
+        } else {
+          cost <- determine_visit_cost(property="norm", prop_index=which(properties_that_can_be_bought$pos %in% current_board_position[whose_turn]))
+        }
+        
+        account[whose_turn] <- account[whose_turn] - cost
+        account[whose_property] <- account[whose_property] + cost
+        
+        cat("The cost amounts to", cost, ", player", whose_property, "has now", account[whose_property], "and player ", whose_turn, "has now", account[whose_turn], "\n")
       }
     }        
     
